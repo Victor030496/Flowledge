7f083ce2ad33e31b422813dfb65641282003d2dc
Victor030496 <victorsame3@hotmail.com>
2018-12-23 21:09:23 -0600

    Menunavfinal2

--- app/src/main/java/mobile/una/com/flowledge/MainActivity.java
+++ app/src/main/java/mobile/una/com/flowledge/MainActivity.java
@@ -1,13 +1,42 @@
 package mobile.una.com.flowledge;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.NavBot);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.bottombaritem_home:
+                                TextView tv1 = (TextView)findViewById(R.id.Prueba);
+                                tv1.setText("Hello this is Home");
+                                return true;
+                            case R.id.bottombaritem_profile:
+                                TextView tv2 = (TextView)findViewById(R.id.Prueba);
+                                tv2.setText("Hello this is profile");
+                                return true;
+
+                        }
+                        return false;
+                    }
+                });
+
     }
+
+
 }
