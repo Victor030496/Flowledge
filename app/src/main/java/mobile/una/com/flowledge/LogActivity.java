a3d66a69e42595d88be0cd95fb3042c50a1a222c
victo <victorsame34@gmail.com>
2018-12-28 13:14:51 -0600

    Correo google

---
+++ app/src/main/java/mobile/una/com/flowledge/LogActivity.java
@@ -0,0 +1,160 @@
+package mobile.una.com.flowledge;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.signin.SignIn;
+
+public class LogActivity extends AppCompatActivity implements View.OnClickListener ,GoogleApiClient.OnConnectionFailedListener {
+    private BottomNavigationView bottomNavigationView;
+
+    private LinearLayout info;
+    private Button out;
+    private SignInButton in;
+    private TextView name,email;
+    private ImageView imagen;
+    private GoogleApiClient googleApiClient;
+    private static final int REQ_CODE=9001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        setTitle("LOG IN");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.NavBot);
+        info = findViewById(R.id.contenedor);
+        out = findViewById(R.id.boton);
+        in = findViewById(R.id.in);
+        name = findViewById(R.id.nombre);
+        email = findViewById(R.id.correo);
+        imagen = findViewById(R.id.imagen);
+        in.setOnClickListener(this);
+        out.setOnClickListener(this);
+        info.setVisibility(View.GONE);
+        GoogleSignInOptions signInOptions=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient= new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,signInOptions).build();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                        switch (item.getItemId()) {
+
+                            case R.id.bottombaritem_home:
+                                finish();
+                                overridePendingTransition(0, 0);
+                                startActivity(new Intent(getBaseContext(), LogActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_NO_ANIMATION));
+                                overridePendingTransition(0, 0);
+                                return true;
+                            case R.id.bottombaritem_profile:
+                                startActivity(new Intent(getBaseContext(), UserActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_NO_ANIMATION));
+                                finish();
+
+                                return true;
+
+                            case R.id.bottombaritem_reply:
+                                finish();
+                                overridePendingTransition(0, 0);
+                                startActivity(new Intent(getBaseContext(), ReplyActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_NO_ANIMATION));
+                                overridePendingTransition(0, 0);
+                                return true;
+
+                        }
+                        return false;
+                    }
+                });
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.in:
+                signIn();
+                break;
+
+            case R.id.boton:
+                signOut();
+                break;
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private void signIn(){
+        Intent intent= Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(intent,REQ_CODE);
+    }
+
+    private void signOut(){
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                updateUI(false);
+            }
+        });
+    }
+
+    private void handleResult(GoogleSignInResult result){
+        if(result.isSuccess()) {
+            GoogleSignInAccount account = result.getSignInAccount();
+            String names = account.getDisplayName();
+            String emails = account.getEmail();
+            String img_url = account.getPhotoUrl().toString();
+            name.setText(names);
+            email.setText(emails);
+            Glide.with(this).load(img_url).into(imagen);
+            updateUI(true);
+        }
+        else{updateUI(false);}
+    }
+
+    private void updateUI(boolean isLogin){
+        if(isLogin){
+            info.setVisibility(View.VISIBLE);
+            in.setVisibility(View.GONE);
+        }
+        else{
+            info.setVisibility(View.GONE);
+            in.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==REQ_CODE){
+            GoogleSignInResult result=Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleResult(result);
+        }
+    }
+}
