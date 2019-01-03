49c2c98b6200dba0cf2b0a2cdcc24fc161f76155
Luis <luisbogants27@gmail.com>
2019-01-03 00:51:08 -0600

    fdsfvdsvdsv

--- app/src/main/java/mobile/una/com/flowledge/MainActivity.java
+++ app/src/main/java/mobile/una/com/flowledge/MainActivity.java
@@ -1,17 +1,23 @@
 package mobile.una.com.flowledge;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 import static java.lang.Boolean.TRUE;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +27,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.NavBot);
+        btn1 = (Button) findViewById(R.id.arboles);
+        btn2  = (Button) findViewById(R.id.listas);
+        btn3= (Button) findViewById(R.id.bd);
+        btn4= (Button) findViewById(R.id.redes);
 
+
+
+        /*btn1.setTextColor(Color.parseColor("#9E9E9E"));
+        btn2.setTextColor(Color.parseColor("#9E9E9E"));
+        btn3.setTextColor(Color.parseColor("#9E9E9E"));
+        btn4.setTextColor(Color.parseColor("#9E9E9E"));*/
+        btn1.setBackgroundColor(Color.parseColor("#8080ff"));
+        btn2.setBackgroundColor(Color.parseColor("#8080ff"));
+        btn3.setBackgroundColor(Color.parseColor("#8080ff"));
+        btn4.setBackgroundColor(Color.parseColor("#8080ff"));
 
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
