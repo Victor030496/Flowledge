1455e7e35bd81e20c28d306e3bece7f43546bd78
Luis <luisbogants27@gmail.com>
2019-01-09 00:17:00 -0600

    viva la weed

---
+++ app/src/main/java/mobile/una/com/flowledge/Adapter2.java
@@ -0,0 +1,69 @@
+package mobile.una.com.flowledge;
+
+/**
+ * Created by Luis Bogantes on 08/01/2019.
+ */
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import mobile.una.com.flowledge.model.Question;
+
+/**
+ * Created by Luis Bogantes on 08/01/2019.
+ */
+
+public class Adapter2 extends BaseAdapter {
+
+    Context contexto;
+    List<Question> preguntas;
+
+
+    public Adapter2(Context contexto, List<Question> preguntas) {
+        this.contexto = contexto;
+        this.preguntas = preguntas;
+    }
+
+    @Override
+    public int getCount() {
+        return preguntas.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return preguntas.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View vista = convertView;
+        LayoutInflater inflat= LayoutInflater.from(contexto);
+        vista =inflat.inflate(R.layout.activity_reply2,null);
+
+        ImageView imagen =(ImageView) vista.findViewById(R.id.imgItem);
+        TextView txt1 =(TextView) vista.findViewById(R.id.txtTitle);
+        TextView txt2 =(TextView) vista.findViewById(R.id.txtDescription);
+
+
+        txt1.setText(preguntas.get(position).getUserNickname().toString());
+        txt2.setText(preguntas.get(position).getDescription().toString());
+        //imagen.setImageResource();
+
+        return vista;
+    }
+
+
+}
+
